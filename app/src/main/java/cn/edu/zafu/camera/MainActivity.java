32c1892b53fbf192c2431a2a4b548ff7f52a12b5
李樟取 <lizhangqu@weidian.com>
2017-07-13 11:44:32 +0800

    * update okhttp and coreprogress

--- app/src/main/java/cn/edu/zafu/camera/MainActivity.java
+++ app/src/main/java/cn/edu/zafu/camera/MainActivity.java
@@ -17,15 +17,6 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.squareup.okhttp.Callback;
-import com.squareup.okhttp.Headers;
-import com.squareup.okhttp.MediaType;
-import com.squareup.okhttp.MultipartBuilder;
-import com.squareup.okhttp.OkHttpClient;
-import com.squareup.okhttp.Request;
-import com.squareup.okhttp.RequestBody;
-import com.squareup.okhttp.Response;
-
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
@@ -36,8 +27,17 @@ import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
 import cn.edu.zafu.camera.activity.CameraActivity;
-import cn.edu.zafu.coreprogress.helper.ProgressHelper;
-import cn.edu.zafu.coreprogress.listener.impl.UIProgressRequestListener;
+import io.github.lizhangqu.coreprogress.ProgressHelper;
+import io.github.lizhangqu.coreprogress.ProgressUIListener;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Headers;
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 
 /**
@@ -46,25 +46,26 @@ import cn.edu.zafu.coreprogress.listener.impl.UIProgressRequestListener;
  * Time: 13:49
  */
 public class MainActivity extends Activity {
-    OkHttpClient mOkHttpClient=new OkHttpClient();
+    OkHttpClient mOkHttpClient;
     private Button mCatch, mRecognize;
-    private EditText mPath,mName,mType;
+    private EditText mPath, mName, mType;
     private ImageView mPhoto;
     private TextView mTextView;
     private ProgressBar mProgressBar;
     private String mPhotoPath;
-    private static final int UPDATE_TEXTVIEW=1;
-    private Handler mHandler=new Handler(){
+    private static final int UPDATE_TEXTVIEW = 1;
+    private Handler mHandler = new Handler() {
         @Override
         public void handleMessage(Message msg) {
             super.handleMessage(msg);
-            switch (msg.what){
+            switch (msg.what) {
                 case UPDATE_TEXTVIEW:
                     mTextView.setText(msg.obj.toString());
                     break;
             }
         }
     };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -104,27 +105,27 @@ public class MainActivity extends Activity {
     }
 
     private void uploadAndRecognize() {
-        if (!TextUtils.isEmpty(mPhotoPath)){
-            File file=new File(mPhotoPath);
+        if (!TextUtils.isEmpty(mPhotoPath)) {
+            File file = new File(mPhotoPath);
             //构造上传请求，类似web表单
-            RequestBody requestBody = new MultipartBuilder().type(MultipartBuilder.FORM)
+            RequestBody requestBody = new MultipartBody.Builder().setType(MultipartBody.FORM)
                     .addPart(Headers.of("Content-Disposition", "form-data; name=\"callbackurl\""), RequestBody.create(null, "/idcard/"))
                     .addPart(Headers.of("Content-Disposition", "form-data; name=\"action\""), RequestBody.create(null, "idcard"))
                     .addPart(Headers.of("Content-Disposition", "form-data; name=\"img\"; filename=\"idcardFront_user.jpg\""), RequestBody.create(MediaType.parse("image/jpeg"), file))
                     .build();
             //这个是ui线程回调，可直接操作UI
-            final UIProgressRequestListener uiProgressRequestListener = new UIProgressRequestListener() {
+            RequestBody progressRequestBody = ProgressHelper.withProgress(requestBody, new ProgressUIListener() {
                 @Override
-                public void onUIRequestProgress(long bytesWrite, long contentLength, boolean done) {
-                    Log.e("TAG", "bytesWrite:" + bytesWrite);
-                    Log.e("TAG", "contentLength" + contentLength);
-                    Log.e("TAG", (100 * bytesWrite) / contentLength + " % done ");
-                    Log.e("TAG", "done:" + done);
+                public void onUIProgressChanged(long numBytes, long totalBytes, float percent, float speed) {
+                    Log.e("TAG", "numBytes:" + numBytes);
+                    Log.e("TAG", "totalBytes" + totalBytes);
+                    Log.e("TAG", percent * 100 + " % done ");
+                    Log.e("TAG", "done:" + (percent >= 1.0));
                     Log.e("TAG", "================================");
                     //ui层回调
-                    mProgressBar.setProgress((int) ((100 * bytesWrite) / contentLength));
+                    mProgressBar.setProgress((int) (100 * percent));
                 }
-            };
+            });
             //进行包装，使其支持进度回调
             final Request request = new Request.Builder()
                     .header("Host", "ocr.ccyunmai.com")
@@ -132,31 +133,32 @@ public class MainActivity extends Activity {
                     .header("Referer", "http://ocr.ccyunmai.com/idcard/")
                     .header("User-Agent", "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2398.0 Safari/537.36")
                     .url("http://ocr.ccyunmai.com/UploadImg.action")
-                    .post(ProgressHelper.addProgressRequestListener(requestBody, uiProgressRequestListener))
+                    .post(progressRequestBody)
                     .build();
             //开始请求
             mOkHttpClient.newCall(request).enqueue(new Callback() {
                 @Override
-                public void onFailure(Request request, IOException e) {
-                    Log.e("TAG", "error");
+                public void onFailure(Call call, IOException e) {
+
                 }
+
                 @Override
-                public void onResponse(Response response) throws IOException {
-                    String result=response.body().string();
+                public void onResponse(Call call, Response response) throws IOException {
+                    String result = response.body().string();
                     Document parse = Jsoup.parse(result);
                     Elements select = parse.select("div.left fieldset");
-                    Log.e("TAG",select.text());
+                    Log.e("TAG", select.text());
                     Document parse1 = Jsoup.parse(select.text());
-                    StringBuilder builder=new StringBuilder();
-                    String name=parse1.select("name").text();
-                    String cardno=parse1.select("cardno").text();
-                    String sex=parse1.select("sex").text();
-                    String folk=parse1.select("folk").text();
-                    String birthday=parse1.select("birthday").text();
-                    String address=parse1.select("address").text();
-                    String issue_authority=parse1.select("issue_authority").text();
-                    String valid_period=parse1.select("valid_period").text();
-                    builder.append("name:"+name)
+                    StringBuilder builder = new StringBuilder();
+                    String name = parse1.select("name").text();
+                    String cardno = parse1.select("cardno").text();
+                    String sex = parse1.select("sex").text();
+                    String folk = parse1.select("folk").text();
+                    String birthday = parse1.select("birthday").text();
+                    String address = parse1.select("address").text();
+                    String issue_authority = parse1.select("issue_authority").text();
+                    String valid_period = parse1.select("valid_period").text();
+                    builder.append("name:" + name)
                             .append("\n")
                             .append("cardno:" + cardno)
                             .append("\n")
@@ -173,16 +175,16 @@ public class MainActivity extends Activity {
                             .append("valid_period:" + valid_period)
                             .append("\n");
                     Log.e("TAG", "name:" + name);
-                    Log.e("TAG","cardno:"+cardno);
-                    Log.e("TAG","sex:"+sex);
-                    Log.e("TAG","folk:"+folk);
-                    Log.e("TAG","birthday:"+birthday);
-                    Log.e("TAG","address:"+address);
-                    Log.e("TAG","issue_authority:"+issue_authority);
-                    Log.e("TAG","valid_period:"+valid_period);
+                    Log.e("TAG", "cardno:" + cardno);
+                    Log.e("TAG", "sex:" + sex);
+                    Log.e("TAG", "folk:" + folk);
+                    Log.e("TAG", "birthday:" + birthday);
+                    Log.e("TAG", "address:" + address);
+                    Log.e("TAG", "issue_authority:" + issue_authority);
+                    Log.e("TAG", "valid_period:" + valid_period);
                     Message obtain = Message.obtain();
-                    obtain.what=UPDATE_TEXTVIEW;
-                    obtain.obj=builder.toString();
+                    obtain.what = UPDATE_TEXTVIEW;
+                    obtain.obj = builder.toString();
                     mHandler.sendMessage(obtain);
                 }
             });
@@ -190,36 +192,38 @@ public class MainActivity extends Activity {
     }
 
     private void initView() {
-        mPath= (EditText) findViewById(R.id.path);
-        mName= (EditText) findViewById(R.id.name);
-        mType= (EditText) findViewById(R.id.type);
+        mPath = (EditText) findViewById(R.id.path);
+        mName = (EditText) findViewById(R.id.name);
+        mType = (EditText) findViewById(R.id.type);
         mCatch = (Button) findViewById(R.id.btn);
         mRecognize = (Button) findViewById(R.id.recognize);
         mTextView = (TextView) findViewById(R.id.tv);
         mPhoto = (ImageView) findViewById(R.id.photo);
-        mProgressBar= (ProgressBar) findViewById(R.id.upload_progress);
+        mProgressBar = (ProgressBar) findViewById(R.id.upload_progress);
         //File file=getFilesDir();
-        File externalFile=getExternalFilesDir("/idcard/");
+        File externalFile = getExternalFilesDir("/idcard/");
         mPath.setText(externalFile.getAbsolutePath());
         Log.e("TAG", externalFile.getAbsolutePath() + "\n" + externalFile.getAbsolutePath());
     }
 
     private void initClient() {
-        mOkHttpClient.setConnectTimeout(1000, TimeUnit.MINUTES);
-        mOkHttpClient.setReadTimeout(1000, TimeUnit.MINUTES);
-        mOkHttpClient.setWriteTimeout(1000, TimeUnit.MINUTES);
+        mOkHttpClient = new OkHttpClient.Builder()
+                .connectTimeout(1000, TimeUnit.MINUTES)
+                .readTimeout(1000, TimeUnit.MINUTES)
+                .writeTimeout(1000, TimeUnit.MINUTES)
+                .build();
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        Log.e("TAG","onActivityResult");
+        Log.e("TAG", "onActivityResult");
         if (requestCode == 100) {
             if (resultCode == RESULT_OK) {
                 Bundle extras = data.getExtras();
-                String path=extras.getString("path");
-                String type=extras.getString("type");
-                Toast.makeText(getApplicationContext(),"path:"+ path + " type:" + type, Toast.LENGTH_LONG).show();
-                mPhotoPath =path;
+                String path = extras.getString("path");
+                String type = extras.getString("type");
+                Toast.makeText(getApplicationContext(), "path:" + path + " type:" + type, Toast.LENGTH_LONG).show();
+                mPhotoPath = path;
                 File file = new File(path);
                 FileInputStream inStream = null;
                 try {
